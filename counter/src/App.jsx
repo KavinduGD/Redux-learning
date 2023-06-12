83fdd6e279cf88da34139de8c7c37376e0cc661b
it21038914 <it21038914@my.sliit.lk>
2023-06-12 23:57:37 +0530

    finished

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,5 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
-  return <>Click on the Vite and React logos to learn more</>;
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>counter= {counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
--- counter/src/store/index.js
+++ counter/src/store/index.js
@@ -1,6 +1,16 @@
 import { legacy_createStore as createStore } from "redux";
 
-const reducer = (state = { counter: 0 }, action) => {};
+const reducer = (state = { counter: 0 }, action) => {
+  //only use synchronous code
+  //we should not mutate the original state
+  if (action.type == "INCREMENT") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type == "DECREMENT") {
+    return { counter: state.counter - 1 };
+  }
+  return state;
+};
 
 const store = createStore(reducer);
 
